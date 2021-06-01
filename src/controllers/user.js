709c99dee99ffcf3cadef24aab4d0651789a744e
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-05-31 22:26:24 -0400

    Get projects while reauthenticating

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -24,8 +24,9 @@ function decodeToken(token) {
  * @returns {Object} user token
  */
 export const reauthenticateUser = async (token) => {
-  const decodedToken = decodeToken(token);
-  const user = await UserModel.findOne({ _id: decodedToken.sub });
+  const { sub } = decodeToken(token);
+  let user = await UserModel.findById(sub);
+  if (user) user = await user.populate('projects').execPopulate();
   return { token, user };
 };
 
@@ -107,6 +108,7 @@ export const deleteUser = async (id) => {
  * @returns {Promise<UserModel>} promise that resolves to user object or error
  */
 export const updateUser = async (id, fields) => {
+  console.log('fields:', fields);
   try {
     const user = await UserModel.findByIdAndUpdate({ _id: id }, fields, { new: true });
     return user;
